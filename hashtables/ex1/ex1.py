06672153d9d9e8efd3c3cf432ba473bdec25e8fb
Carlos Sanchez <carlo.s16@hotmail.com>
2020-07-18 12:50:11 -0400

    finished first one

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -3,5 +3,14 @@ def get_indices_of_item_weights(weights, length, limit):
     YOUR CODE HERE
     """
     # Your code here
+    dict = {}
 
+    for weight_one, weight_of_item in enumerate(weights):
+        try:
+            weight_two = dict[limit - weight_of_item]
+        except:
+            dict[weight_of_item] = weight_one
+        else:
+            return tuple([weight_one, weight_two])
+        
     return None
